0e408755ea1bb062e34e7f9256c160b0da562292
douglas0994 <douglas0994@outlook.com>
2022-12-14 22:20:51 -0300

    corrigido links e imagens

--- js/script.js
+++ js/script.js
@@ -1,14 +1,58 @@
+function initTab() {
+	const tabMenu = document.querySelectorAll('.js-tabmenu li')
+	const tabContent = document.querySelectorAll('.js-tabcontent section')
+	tabContent[0].classList.add('ativo');
+	
+	if(tabContent.length && tabMenu.length) {
+	 
+		function activeTab(index) {
+			tabContent.forEach((item) => {
+				item.classList.remove('ativo')
+			});
+			tabMenu.forEach((item) => {
+				item.classList.remove('ativo')
+			});
+			tabContent[index].classList.add('ativo')
+			tabMenu[index].classList.add('ativo')
+		}
+	
+		tabMenu.forEach((item, index) => {
+			item.addEventListener('click', () => { 
+				activeTab(index) 
+			})
+		})
+	}
+}
+
+initTab();
+
+
+function initScroll() {
+	const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+	function scrollToSection(event) {
+		event.preventDefault();
+		const href = event.currentTarget.getAttribute('href');
+		const section = document.querySelector(href);
+		section.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+	}
+	linksInternos.forEach((link) => {
+		link.addEventListener('click', scrollToSection)
+	})
+}
+
+initScroll();
+
+
+
 if (window.SimpleSlide) {
 
 	new SimpleSlide({
-		slide: "quote", // nome do atributo data-slide="principal"
-		time: 5000 // tempo de transição dos slides
-	});
-
-	new SimpleSlide({
-		slide: "portfolio",
-		time: 5000,
-		nav: true
+		slide: "quote",
+		time: 5000 
 	});
 }
 
